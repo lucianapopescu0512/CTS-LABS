5ac83c0a17fec80e5367f0b152584a36fae652db
lucianapopescu0512 <72069124+lucianapopescu0512@users.noreply.github.com>
2021-04-17 10:08:17 +0300

    composite

---
+++ seminar8/src/ro/ase/cts/composite/clase/ComponentaMeniu.java
@@ -0,0 +1,22 @@
+package ro.ase.cts.composite.clase;
+
+public abstract class ComponentaMeniu {
+
+	private String nume;
+	
+	public abstract void afisareDescriere();
+	public abstract ComponentaMeniu getComponenta(int poz) throws Exception;
+	public abstract void adaugaComponenta(ComponentaMeniu componenta);
+	public abstract void stergeComponenta(ComponentaMeniu componenta);
+	
+	public ComponentaMeniu(String nume) {
+		super();
+		this.nume = nume;
+	}
+	public String getNume() {
+		return nume;
+	}
+	
+	
+	
+}
