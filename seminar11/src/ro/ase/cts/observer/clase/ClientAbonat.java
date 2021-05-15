be26e42ff474e3a48a9b7063ce983bbf490cbe84
lucianapopescu0512 <72069124+lucianapopescu0512@users.noreply.github.com>
2021-05-15 10:01:48 +0300

    observer

---
+++ seminar11/src/ro/ase/cts/observer/clase/ClientAbonat.java
@@ -0,0 +1,19 @@
+package ro.ase.cts.observer.clase;
+
+public class ClientAbonat implements Observer{
+
+	private String numeClient;
+	
+	
+	public ClientAbonat(String numeClient) {
+		super();
+		this.numeClient = numeClient;
+	}
+
+	@Override
+	public void primesteAnunt(String mesaj) {
+		System.out.println(this.numeClient+" ai primit anuntul "+mesaj);
+		
+	}
+
+}
